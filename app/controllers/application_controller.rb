74e3f18735da640d29ee0d2de906485fbbd04f3e
vagrant <vagrant@localhost.localdomain>
2020-11-19 17:24:56 +0000

    admin topページおよび各ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    if user_signed_in?
+       root_path(resource)
+    else
+        admin_top_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :postcode, :prefecture_code, :address_city, :address_street, :address_building, :phone_number])
   end
