9bcbe517d5031cb19d3da1c5d5e254ac9b9fbc11
vagrant <vagrant@localhost.localdomain>
2020-11-27 14:55:38 +0000

    商品登録機能実装　会員側に商品表示

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -25,6 +25,18 @@ class Admin::ProductsController < ApplicationController
 	  @product = Product.find(params[:id])
 	end
 
+	def update
+	  @product = Product.find(params[:id])
+	  @product.update(product_params)
+	  redirect_to admin_products_path
+	end
+
+	def destroy
+	  @product = Product.find(params[:id])
+	  @product.destroy
+	  redirect_to admin_products_path
+	end
+	
 	private
 
 	def product_params
